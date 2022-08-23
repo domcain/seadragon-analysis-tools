b3771f89851ead2cb1d267dc1d23d17f8d1b0ab9
hannahdoret <22846377@student.uwa.edu.au>
2022-08-23 12:41:02 +0800

    Removed unnecessary panel

--- interface.py
+++ interface.py
@@ -4,21 +4,18 @@ from tkinter import filedialog
 
 root = Tk()
 root.title("Seadragon Search Data Analysis Tool")
-root.geometry("600x550+100-100")
+root.geometry("600x600+100-50")
 
 #design is yet to be finalised, just trying make the panels...
 
-#Create main panel to contain further divisions
-main_panel = PanedWindow(orient=VERTICAL, bd=0, width=600, height=550)
-main_panel.pack(fill=BOTH, expand=1)
 #Create top panel
-top_panel = PanedWindow(main_panel, bd=1, relief="groove", height=100, bg="#0ae8cd")
-main_panel.add(top_panel)
+top_panel = PanedWindow(bd=1, relief="groove", height=100, bg="#0ae8cd")
+top_panel.pack(fill=BOTH, expand=1)
 #Create middle panel
-middle_panel = PanedWindow(main_panel, bd=1, relief="groove", height=300, bg="#fdfd34")
-main_panel.add(middle_panel)
+middle_panel = PanedWindow(bd=1, relief="groove", height=400, bg="#fdfd34")
+middle_panel.pack(fill=BOTH, expand=1)
 #Create bottom panel
-bottom_panel = PanedWindow(main_panel, bd=1, relief="groove", height=150, bg="#0ae8cd")
-main_panel.add(bottom_panel)
+bottom_panel = PanedWindow(bd=1, relief="groove", height=100, bg="#0ae8cd")
+bottom_panel.pack(fill=BOTH, expand=1)
 
 root.mainloop()
