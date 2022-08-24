1d46b351ed98613efe7a976da359e47e18a948ea
hannahdoret <22846377@student.uwa.edu.au>
2022-08-24 22:17:05 +0800

    Fixed frame placing issue with grid_propograte()

--- frames.py
+++ frames.py
@@ -16,12 +16,23 @@ root.iconbitmap('seahorse.ico')
 # root.rowconfigure(1, weight = 8)
 # root.rowconfigure(2, weight = 1)
 
-topFrame = Frame(root, height = 90, width = 960, bg = "#0ae8cd").grid(row = 0)
-midFrame = Frame(root, height = 300, width = 960, bg = "#F8FF00").grid(row = 1)
-botFrame = Frame(root, height  = 90, width = 960, bg = "#0ae8cd").grid(row = 2)
+topFrame = Frame(root, height = 90, width = 960, bg = "#0ae8cd")
+midFrame = Frame(root, height = 300, width = 960, bg = "#F8FF00")
+botFrame = Frame(root, height  = 90, width = 960, bg = "#0ae8cd")
+
+topFrame.grid(row = 0)
+midFrame.grid(row = 1)
+botFrame.grid(row = 2)
+
+topFrame.grid_propagate(0)
+midFrame.grid_propagate(0)
+botFrame.grid_propagate(0)
+
 
 #this lable seems to be bounded to the parent window and not the frame despite me specifically mentioning the master to be the frame and not the parent window 
 #will keep looking into this 
+
+
 submit = Label(midFrame, text = "Submit").grid(row = 0, column = 0)
 
 root.mainloop()
