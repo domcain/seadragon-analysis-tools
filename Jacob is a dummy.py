afb87c80acad4d7588b0842b6c790b06053986a8
Aidan Smith <aidan.w.smith@outlook.com>
2022-10-07 23:23:28 +0800

    debug statement

---
+++ Jacob is a dummy.py
@@ -0,0 +1,6 @@
+import xlrd as xl
+
+# "C:/Users/aidan/OneDrive/UWA/2022 Sem 2/CITS3200/seadragon-analysis-tools/Excel version of iNat sample.xlsx"
+
+sds_wb = xl.open_workbook("C:/Users/aidan/OneDrive/UWA/2022 Sem 2/CITS3200/Seadragon Search sample.xls")
+print("WORKED")
